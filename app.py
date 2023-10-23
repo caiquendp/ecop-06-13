33bdc1aa8e78e73f6519590b7a5542d2956baa0f
caiquendp <148247223+caiquendp@users.noreply.github.com>
2023-10-23 11:53:49 +0000

    atualizando tudo

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config('kiq-Ecop06',
+    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png',
+    )
+
+st.title('Pagina alfa Ecop06')
+
+esports = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+
+st.dataframe(esports)
